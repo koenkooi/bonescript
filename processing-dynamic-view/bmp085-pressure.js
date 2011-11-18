2c3f8a05da8ff024b3cb860fbbf6a69d36e298c7
Koen Kooi <k-kooi@ti.com>
2011-11-18 12:32:31 +0100

    processing-dynamic-view: draw miliBar numbers
    Signed-off-by: Koen Kooi <k-kooi@ti.com>

--- processing-dynamic-view/bmp085-pressure.js
+++ processing-dynamic-view/bmp085-pressure.js
@@ -1,6 +1,6 @@
 exports.pressureConfig = {
     file: "/sys/bus/i2c/drivers/bmp085/3-0077/pressure0_input",
-    unit: "milibar",
+    unit: "miliBar",
     delay: 5000,
     scale: 100,
     rangeHigh: 1100,
