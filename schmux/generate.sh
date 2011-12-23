3b9f9e692c71194909644cdd733e06b90bec8e67
Koen Kooi <k-kooi@ti.com>
2011-12-23 21:41:40 +0100

    schmux: fix table generator, experiment with loops
    Signed-off-by: Koen Kooi <k-kooi@ti.com>

--- schmux/generate.sh
+++ schmux/generate.sh
@@ -6,7 +6,7 @@ echo "<body><table>"
 for i in $(seq 1 2 46) ; do
 	echo "<tr><td id='P9_${i}_name'></td><td id='P9_$i' class='pin'></td><td id='P9_$(expr $i + 1)' class='pin'></td><td id='P9_$(expr $i + 1)_name'></td>"
 	echo "<td class='spacer'></td>"
-	echo "<td id='P8_${i}_name'></td><td id='P8_$i' class='pin'></td><td id='P8_$(expr $i + 1)' class='pin'></td id='P8_$(expr $i + 1)_name'><td></td></tr>"
+	echo "<td id='P8_${i}_name'></td><td id='P8_$i' class='pin'></td><td id='P8_$(expr $i + 1)' class='pin'></td><td id='P8_$(expr $i + 1)_name'></td></tr>"
 done
 echo "</table></body></html>"
 
